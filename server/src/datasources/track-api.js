39e9d3bf204f08f62e5a4ff6205806c6e70448d8
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-11-25 12:11:42 +0300

    Create RESTDataSource

---
+++ server/src/datasources/track-api.js
@@ -0,0 +1,21 @@
+// import apollo-datasource-rest package
+const { RESTDataSource } = require('apollo-datasource-rest');
+
+// Extend a RESTDataSource class
+class TrackAPI extends RESTDataSource {
+  // Define a constructor() method
+  constructor() {
+    super();
+    this.baseURL = 'https://odyssey-lift-off-rest-api.herokuapp.com/';
+  }
+
+  getTracksForHome() {
+    return this.get('tracks');
+  }
+
+  getAuthor(authorId) {
+    return this.get(`author/{authorId}`);
+  }
+}
+
+module.exports = TrackAPI;
