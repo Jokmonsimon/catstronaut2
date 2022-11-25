64cdb36042ad823b444ba96e39c8929397790bbc
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-11-25 18:12:08 +0300

    Connect dataSources

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
-const TrackAPI = reuire('./datasources/track-api');
+const TrackAPI = require('./datasources/track-api');
 
 const server = new ApolloServer({
   typeDefs,
